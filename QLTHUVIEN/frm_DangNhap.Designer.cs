8c684bdedd5cf3c92b0650f2a10aa8c45c2b6577
smoker9988 <huydanghttt@gmail.com>
2017-11-10 15:55:34 +0700

    aSDFG

--- QLTHUVIEN/frm_DangNhap.Designer.cs
+++ QLTHUVIEN/frm_DangNhap.Designer.cs
@@ -29,7 +29,18 @@
         private void InitializeComponent()
         {
             this.labelX1 = new DevComponents.DotNetBar.LabelX();
-            this.comboBoxEx1 = new DevComponents.DotNetBar.Controls.ComboBoxEx();
+            this.cbAuthentication = new DevComponents.DotNetBar.Controls.ComboBoxEx();
+            this.labelX2 = new DevComponents.DotNetBar.LabelX();
+            this.cb_Authentication = new DevComponents.DotNetBar.LabelX();
+            this.labelX4 = new DevComponents.DotNetBar.LabelX();
+            this.labelX5 = new DevComponents.DotNetBar.LabelX();
+            this.txtServerName = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.textBoxX2 = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.textBoxX3 = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.btDangnhap = new DevComponents.DotNetBar.ButtonX();
+            this.buttonX2 = new DevComponents.DotNetBar.ButtonX();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // labelX1
@@ -41,25 +52,138 @@
             this.labelX1.TabIndex = 0;
             this.labelX1.Text = "Connect to Server ...";
             // 
-            // comboBoxEx1
+            // cbAuthentication
             // 
-            this.comboBoxEx1.DisplayMember = "Text";
-            this.comboBoxEx1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            this.comboBoxEx1.FormattingEnabled = true;
-            this.comboBoxEx1.Location = new System.Drawing.Point(217, 82);
-            this.comboBoxEx1.Name = "comboBoxEx1";
-            this.comboBoxEx1.Size = new System.Drawing.Size(121, 21);
-            this.comboBoxEx1.TabIndex = 1;
+            this.cbAuthentication.DisplayMember = "Text";
+            this.cbAuthentication.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cbAuthentication.FormattingEnabled = true;
+            this.cbAuthentication.Location = new System.Drawing.Point(184, 69);
+            this.cbAuthentication.Name = "cbAuthentication";
+            this.cbAuthentication.Size = new System.Drawing.Size(121, 21);
+            this.cbAuthentication.TabIndex = 1;
+            this.cbAuthentication.SelectedIndexChanged += new System.EventHandler(this.cbAuthentication_SelectedIndexChanged);
+            // 
+            // labelX2
+            // 
+            this.labelX2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.labelX2.Location = new System.Drawing.Point(41, 19);
+            this.labelX2.Name = "labelX2";
+            this.labelX2.Size = new System.Drawing.Size(91, 23);
+            this.labelX2.TabIndex = 5;
+            this.labelX2.Text = "Server Name";
+            this.labelX2.Click += new System.EventHandler(this.labelX2_Click);
+            // 
+            // cb_Authentication
+            // 
+            this.cb_Authentication.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.cb_Authentication.Location = new System.Drawing.Point(41, 69);
+            this.cb_Authentication.Name = "cb_Authentication";
+            this.cb_Authentication.Size = new System.Drawing.Size(91, 23);
+            this.cb_Authentication.TabIndex = 6;
+            this.cb_Authentication.Text = "Authentication";
+            this.cb_Authentication.Click += new System.EventHandler(this.labelX3_Click);
+            // 
+            // labelX4
+            // 
+            this.labelX4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.labelX4.Location = new System.Drawing.Point(41, 117);
+            this.labelX4.Name = "labelX4";
+            this.labelX4.Size = new System.Drawing.Size(75, 23);
+            this.labelX4.TabIndex = 7;
+            this.labelX4.Text = "User Name";
+            // 
+            // labelX5
+            // 
+            this.labelX5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.labelX5.Location = new System.Drawing.Point(41, 176);
+            this.labelX5.Name = "labelX5";
+            this.labelX5.Size = new System.Drawing.Size(75, 23);
+            this.labelX5.TabIndex = 8;
+            this.labelX5.Text = "Password";
+            // 
+            // txtServerName
+            // 
+            // 
+            // 
+            // 
+            this.txtServerName.Border.Class = "TextBoxBorder";
+            this.txtServerName.Location = new System.Drawing.Point(184, 19);
+            this.txtServerName.Name = "txtServerName";
+            this.txtServerName.Size = new System.Drawing.Size(121, 20);
+            this.txtServerName.TabIndex = 9;
+            // 
+            // textBoxX2
+            // 
+            // 
+            // 
+            // 
+            this.textBoxX2.Border.Class = "TextBoxBorder";
+            this.textBoxX2.Location = new System.Drawing.Point(184, 117);
+            this.textBoxX2.Name = "textBoxX2";
+            this.textBoxX2.Size = new System.Drawing.Size(121, 20);
+            this.textBoxX2.TabIndex = 10;
+            this.textBoxX2.TextChanged += new System.EventHandler(this.textBoxX2_TextChanged);
+            // 
+            // textBoxX3
+            // 
+            // 
+            // 
+            // 
+            this.textBoxX3.Border.Class = "TextBoxBorder";
+            this.textBoxX3.Location = new System.Drawing.Point(184, 176);
+            this.textBoxX3.Name = "textBoxX3";
+            this.textBoxX3.Size = new System.Drawing.Size(121, 20);
+            this.textBoxX3.TabIndex = 11;
+            // 
+            // btDangnhap
+            // 
+            this.btDangnhap.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btDangnhap.Location = new System.Drawing.Point(179, 326);
+            this.btDangnhap.Name = "btDangnhap";
+            this.btDangnhap.Size = new System.Drawing.Size(110, 23);
+            this.btDangnhap.TabIndex = 12;
+            this.btDangnhap.Text = "Đăng nhập";
+            this.btDangnhap.Click += new System.EventHandler(this.btDangnhap_Click);
+            // 
+            // buttonX2
+            // 
+            this.buttonX2.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX2.Location = new System.Drawing.Point(340, 326);
+            this.buttonX2.Name = "buttonX2";
+            this.buttonX2.Size = new System.Drawing.Size(106, 23);
+            this.buttonX2.TabIndex = 13;
+            this.buttonX2.Text = "Thoát";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxX3);
+            this.groupBox1.Controls.Add(this.textBoxX2);
+            this.groupBox1.Controls.Add(this.txtServerName);
+            this.groupBox1.Controls.Add(this.labelX5);
+            this.groupBox1.Controls.Add(this.labelX4);
+            this.groupBox1.Controls.Add(this.cb_Authentication);
+            this.groupBox1.Controls.Add(this.labelX2);
+            this.groupBox1.Controls.Add(this.cbAuthentication);
+            this.groupBox1.Location = new System.Drawing.Point(110, 86);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(359, 216);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
             // 
             // frm_DangNhap
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(471, 325);
-            this.Controls.Add(this.comboBoxEx1);
+            this.ClientSize = new System.Drawing.Size(651, 388);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonX2);
+            this.Controls.Add(this.btDangnhap);
             this.Controls.Add(this.labelX1);
             this.Name = "frm_DangNhap";
             this.Text = "frm_DangNhap";
+            this.Load += new System.EventHandler(this.frm_DangNhap_Load);
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -67,6 +191,16 @@
         #endregion
 
         private DevComponents.DotNetBar.LabelX labelX1;
-        private DevComponents.DotNetBar.Controls.ComboBoxEx comboBoxEx1;
+        private DevComponents.DotNetBar.Controls.ComboBoxEx cbAuthentication;
+        private DevComponents.DotNetBar.LabelX labelX2;
+        private DevComponents.DotNetBar.LabelX cb_Authentication;
+        private DevComponents.DotNetBar.LabelX labelX4;
+        private DevComponents.DotNetBar.LabelX labelX5;
+        private DevComponents.DotNetBar.Controls.TextBoxX txtServerName;
+        private DevComponents.DotNetBar.Controls.TextBoxX textBoxX2;
+        private DevComponents.DotNetBar.Controls.TextBoxX textBoxX3;
+        private DevComponents.DotNetBar.ButtonX btDangnhap;
+        private DevComponents.DotNetBar.ButtonX buttonX2;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
--- QLTHUVIEN/frm_DangNhap.cs
+++ QLTHUVIEN/frm_DangNhap.cs
@@ -15,5 +15,57 @@ namespace QLTHUVIEN
         {
             InitializeComponent();
         }
+
+        private void labelX3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelX2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void textBoxX2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frm_DangNhap_Load(object sender, EventArgs e)
+        {
+            txtServerName.text = "localhost";
+            cbAuthentication.SelectedIndex = 0;
+        }
+
+        private void cbAuthentication_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            bool hide = true;
+            if (cbAuthentication == 0)
+                hide = false;
+            txtUserName.Enable = hide;
+            txtPassword.Enable = hide;
+        }
+
+        private void btDangnhap_Click(object sender, EventArgs e)
+        {
+            if (cbAuthentication.SelectedIndex == 0)
+                LOP.XL_BANG.Chuoi_lien_ket = "Data Source=" + txtServerName.Text + ";Initial Catalog=QLTHUVIEN;Intergrated Scurity=True";
+            else
+                LOP.XL_BANG.Chuoi_lien_ket = "Data Source=" + txtServerName.Text + ";Initial Catalog=QLTHUVIEN;User ID=" + txtUserName.Text + ";Password=" + txtPasswoed.text;
+            sqlConnection cnn = new sqlConnection(LOP.XL_BANG.Chuoi_lien_ket);
+            try
+            {
+                cnn.Open();
+                MessageBox.Show("Ket noi thanh cong!");
+                cnn.Close();
+                this.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
