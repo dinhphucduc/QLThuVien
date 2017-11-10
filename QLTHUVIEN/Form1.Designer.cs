0bbd6724820c7e4e04f48aa0a163f6b1f832eef4
Admin <Admin@DESKTOP-NE70A7B>
2017-11-10 13:42:36 +0700

    ok

--- QLTHUVIEN/Form1.Designer.cs
+++ QLTHUVIEN/Form1.Designer.cs
@@ -28,20 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.contextMenuBar1 = new DevComponents.DotNetBar.ContextMenuBar();
+            ((System.ComponentModel.ISupportInitialize)(this.contextMenuBar1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // contextMenuBar1
+            // 
+            this.contextMenuBar1.Location = new System.Drawing.Point(41, 24);
+            this.contextMenuBar1.Name = "contextMenuBar1";
+            this.contextMenuBar1.Size = new System.Drawing.Size(75, 25);
+            this.contextMenuBar1.Style = DevComponents.DotNetBar.eDotNetBarStyle.Office2003;
+            this.contextMenuBar1.TabIndex = 0;
+            this.contextMenuBar1.TabStop = false;
+            this.contextMenuBar1.Text = "contextMenuBar1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(585, 400);
+            this.Controls.Add(this.contextMenuBar1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.contextMenuBar1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private DevComponents.DotNetBar.ContextMenuBar contextMenuBar1;
     }
 }
 
